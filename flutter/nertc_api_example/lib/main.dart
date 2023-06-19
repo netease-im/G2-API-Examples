6950cbbe649d7f03c2165b8bf3e2acf921603170
景致 <jingzhi02@corp.netease.com>
2023-06-19 13:45:47 +0800

    copy right

--- flutter/nertc_api_example/lib/main.dart
+++ flutter/nertc_api_example/lib/main.dart
@@ -1,3 +1,6 @@
+// Copyright (c) 2022 NetEase, Inc. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
 import 'package:flutter/material.dart';
 import 'package:nertc_api_example/AudioCall/audioCallingPage.dart';
 import 'package:nertc_api_example/VideoCall/videoCallingPage.dart';
