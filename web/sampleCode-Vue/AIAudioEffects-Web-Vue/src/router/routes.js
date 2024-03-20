254aa865af64e5eaed57c2ecfa61bc82676446fe
李海峰 <lihaifeng01@corp.netease.com>
2024-03-20 11:06:29 +0800

    同步 g2 web demo 示例源码

---
+++ web/sampleCode-Vue/AIAudioEffects-Web-Vue/src/router/routes.js
@@ -0,0 +1,7 @@
+import AIAudioEffects from '../views/AIAudioEffects/index.vue'
+
+export default [{
+  path: '/AIAudioEffects',
+  name: 'AIAudioEffects',
+  component: AIAudioEffects
+}]
---
+++ web/sampleCode-Vue/AudioDump-Web-Vue/src/router/routes.js
@@ -0,0 +1,7 @@
+import AudioDump from '../views/audioDump/index.vue'
+
+export default [{
+  path: '/audioDump',
+  name: 'audioDump',
+  component: AudioDump
+}]
