5775b4cbc760d3ad21ce435f43e215941c41f473
李海峰 <lihaifeng01@corp.netease.com>
2024-06-14 17:16:49 +0800

    新增远端流美声变声

---
+++ web/sampleCode-Vue/AIAudioEffectsRemote-Web-Vue/src/router/routes.js
@@ -0,0 +1,7 @@
+import AIAudioEffectsRemote from '../views/AIAudioEffectsRemote/index.vue'
+
+export default [{
+  path: '/AIAudioEffectsRemote',
+  name: 'AIAudioEffectsRemote',
+  component: AIAudioEffectsRemote
+}]
