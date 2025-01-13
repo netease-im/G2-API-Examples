dfaea73db48528a5972f89319552d2b5462a22c7
李海峰 <lihaifeng01@corp.netease.com>
2025-01-13 11:26:26 +0800

    点播上传

---
+++ vod-upload/web/SampleCode-React/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import VodUploader from './component/VodUploader';
+
+function App() {
+  return (
+    <div className="App">
+      <VodUploader />
+    </div>
+  );
+}
+
+export default App;
